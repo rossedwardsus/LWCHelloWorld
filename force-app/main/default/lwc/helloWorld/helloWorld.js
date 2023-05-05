63979fc547ecff780708390b2541d1a73e18d77c
Ross Edwards <rossedwards.us@gmail.com>
2023-05-05 12:05:26 -0400

    added some user information  LWC example

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,37 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, wire, track } from 'lwc';
+import Id from '@salesforce/user/Id';
+import { getRecord } from 'lightning/uiRecordApi';
+import UserNameFIELD from '@salesforce/schema/User.Name';
+//import userEmailFIELD from '@salesforce/schema/User.Email';
+//import userIsActiveFIELD from '@salesforce/schema/User.IsActive';
+//import userAliasFIELD from '@salesforce/schema/User.Alias';
+
+
 export default class HelloWorld extends LightningElement {
+  @track error;
+  @track userId = Id;
+  currentUserName;
+  //@track currentUserEmail;
+  //@track currentIsActive;
+  //@track currentUserAlias;
+ 
+
   @track greeting = "World";
   changeHandler(event) {
     this.greeting = event.target.value;
   }
+
+  @wire(getRecord, { recordId: Id, fields: [UserNameFIELD, userEmailFIELD, userIsActiveFIELD, userAliasFIELD ]}) 
+  currentUserInfo({error, data}) {
+        if (data) {
+            this.currentUserName = data.fields.Name.value;
+            //this.currentUserEmail = data.fields.Email.value;
+            //this.currentIsActive = data.fields.IsActive.value;
+            //this.currentUserAlias = data.fields.Alias.value;
+        } else if (error) {
+            this.error = error ;
+        }
+    }
+
+  
 }
